e536a80ac65e8152f907003728f6a0960e14f4f0
Riccardo Corsi <corsi.riccardo@hotmail.it>
2024-11-11 09:36:19 +0100

    GameMode + GameInstance + BPs

---
+++ Asg/Source/Asg/Public/AsgGameInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AsgGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASG_API UAsgGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
---
+++ Asg/Source/Asg/Public/AsgGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "AsgGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class ASG_API AAsgGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
